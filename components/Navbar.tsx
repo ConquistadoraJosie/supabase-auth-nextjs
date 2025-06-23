9347deb6d59df8af3bcd4b7c8a89b23e80425173
almas-josephine <almas.josephine.placides@gmail.com>
2025-06-23 18:11:19 -0400

    Navbar will now show the existing user's email at the top of the navigation bar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,12 @@
+import { createClient } from "@/utils/supabase/server";
 import Link from "next/link";
 import React from "react";
+import Logout from "./Logout";
 
 const Navbar = async () => {
+  // This will update the Navbar to show the user's email if they are logged in
+  const supabase = await createClient();
+  const { data: { user } } = await supabase.auth.getUser();
   return (
     <nav className="border-b bg-background w-full flex items-center">
       <div className="flex w-full items-center justify-between my-4">
@@ -13,11 +18,21 @@ const Navbar = async () => {
           <Link href="/private">Private</Link>
         </div>
         <div className="flex items-center gap-x-5">
-          <Link href="/login">
-            <div className="bg-blue-600 text-white text-sm px-4 py-2 rounded-sm">
-              Login
-            </div>
-          </Link>
+          {/* If no users exists, then it will redirect back to the login page  */}
+          {!user ? (
+            <Link href="/login">
+              <div className="text-sm px-4 py-2 bg-blue-500 text-white rounded-md">
+                Login
+              </div>
+            </Link>
+          ) : (
+            <> {/* If a user exists, then it will show the user's email and a logout button */}
+              <div className="flex items-center gap-x-2 text-sm">
+                {user?.email}
+              </div>
+              <Logout />
+            </>
+          )}
         </div>
       </div>
     </nav>
